f79e7180027a0aec1399c5ddbd54532a3c8f60d6
Dragan Golic <dragangolic@Dragans-MacBook-Pro.local>
2026-06-25 15:09:28 +0200

    data transformation tested

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from NetworkSecurity.Components.data_ingestion import DataIngestion
 from NetworkSecurity.Components.data_validation import DataValidation
+from NetworkSecurity.Components.data_transformation import DataTransformation
+
 from NetworkSecurity.Exception.exception import NetworkSecurityException
 from NetworkSecurity.Logging.logger import logging
-from NetworkSecurity.Entity.config_entity import DataIngestionConfig, DataValidationConfig, TrainingPipelineConfig
+from NetworkSecurity.Entity.config_entity import DataIngestionConfig, DataValidationConfig, TrainingPipelineConfig, DataTransformationConfig
 import sys
 
 
@@ -14,13 +16,21 @@ if __name__ == "__main__":
         data_ingestion = DataIngestion(dataingestionconfig)
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data ingestion completed")
-        print(dataingestionconfig)
+        print(dataingestionartifact)
+
         data_validation_config=DataValidationConfig(trainingpipelineconfig)
         data_validation = DataValidation(dataingestionartifact, data_validation_config)
         logging.info("Starting data validation")
-        data_validation.initiate_data_validation()
+        data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data validation completed")
-        print(data_validation_config)
+        print(data_validation_artifact)
+        
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        logging.info("Starting data transformation")
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data transformation completed")
+        print(data_transformation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
