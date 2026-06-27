fc0b28b6385996ab02d38594152a5bcfceffc4b9
Dragan Golic <dragangolic@Dragans-MacBook-Pro.local>
2026-06-27 18:12:18 +0200

    app.py done, fastapi works

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ import pandas as pd
 from NetworkSecurity.Utils.Main_utils.utils import load_object
 from NetworkSecurity.Constants.training_pipeline import (DATA_INGESTION_DATABASE_NAME, 
                                                          DATA_INGESTION_COLLECTION_NAME)
+from NetworkSecurity.Utils.Ml_utils.Model.estimator import NetworkModel 
 
 ca = certifi.where()
 load_dotenv()
@@ -27,6 +28,10 @@ collection = database[DATA_INGESTION_COLLECTION_NAME]
 
 app = FastAPI()
 origins = ["*"]
+
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="./templates")
+
 
 app.add_middleware(CORSMiddleware,
                    allow_origins = origins,
@@ -45,6 +50,28 @@ async def train_route():
         train_pipeline = TrainingPipeline()
         train_pipeline.run_pipeline()
         return Response("Training is successful")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+@app.get("/predict")
+async def predict_rout(request:Request, file:UploadFile=File(...)):
+    try:
+        df = pd.read_csv(file.file)
+        preprocessor = load_object("final_model/preprocessor.pkl")
+        final_model = load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor = preprocessor, model = final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df['predict_column'] = y_pred
+        print(df['predict_column'])
+        #df['predict_column'].replace(-1, 0)
+        #return df.to_json()
+        df.to_csv("Prediction_output/output.csv")
+        table_html = df.to_html(classes = "table table-striped")
+        #print(table_html)
+        return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
 
